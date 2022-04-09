f4c9dacfe934d0c0295135707cb77758f0ecd6b0
Sergey <ser_8904@mail.ru>
2022-04-09 11:48:59 +0300

    delete unused bootstrap deps for custom css

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,27 +13,20 @@ export default (app) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
     })
-    .get(
-      '/users/:id/edit',
-      { name: 'editUser', preValidation: app.authenticate },
-      async (req, reply) => {
-        const user = await app.objection.models.user.query().findById(req.user.id);
-        reply.render('users/edit', { user });
-        return reply;
-      },
-    )
     .post('/users', { name: 'createUser' }, async (req, reply) => {
+      const user = new app.objection.models.user();
+      user.$set(req.body.data);
+
       try {
         const validUser = await app.objection.models.user.fromJson(req.body.data);
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
-      } catch (error) {
-        const { data } = error;
+      } catch ({ data }) {
         req.flash('error', i18next.t('flash.users.create.error'));
-        reply.statusCode = 422;
-        reply.render('users/new', { user: req.body.data, errors: data });
+        reply.render('users/new', { user, errors: data });
       }
+
       return reply;
     });
 };
