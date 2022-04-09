3aaa89dcb63490ba18bbb070b80d127fa99bf9e8
Sergey <ser_8904@mail.ru>
2022-04-09 16:35:14 +0300

    fix routes naming

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,16 +4,16 @@ import i18next from 'i18next';
 
 export default (app) => {
   app
-    .get('/users', { name: 'users' }, async (req, reply) => {
+    .get('/users', { name: 'users#index' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
       reply.render('users/index', { users });
       return reply;
     })
-    .get('/users/new', { name: 'newUser' }, (req, reply) => {
+    .get('/users/new', { name: 'users#new' }, (req, reply) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
     })
-    .post('/users', { name: 'createUser' }, async (req, reply) => {
+    .post('/users', { name: 'users#create' }, async (req, reply) => {
       const user = new app.objection.models.user();
       user.$set(req.body.data);
 
@@ -21,7 +21,7 @@ export default (app) => {
         const validUser = await app.objection.models.user.fromJson(req.body.data);
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
-        reply.redirect(app.reverse('root'));
+        reply.redirect(app.reverse('root#index'));
       } catch ({ data }) {
         req.flash('error', i18next.t('flash.users.create.error'));
         reply.render('users/new', { user, errors: data });
